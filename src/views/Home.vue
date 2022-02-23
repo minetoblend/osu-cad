1857298c13ba885f1fcda394cc605ebfbd2ceb0b
Marvin Schürz <minetoblend@gmail.com>
2022-02-23 19:00:26 +0100

    add mp3 upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
-
+    <input type="file" accept=".mp3" @input="uploadMp3"/>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import {httpClient} from "@/common-http";
 
 export default defineComponent({
   name: 'Home',
-  components: {
-  },
+  components: {},
   setup() {
+    async function uploadMp3(evt: Event) {
+      const formData = new FormData()
+      formData.append('file', (evt.target as HTMLInputElement).files!.item(0)!)
+
+      await httpClient.post('/beatmap/mp3', formData)
+    }
+
+    return {
+      uploadMp3
+    }
   }
 });
 </script>
