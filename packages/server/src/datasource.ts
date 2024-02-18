61abee1074b9f61c6aa04c7d6fdcfcb6175afbdc
Marvin Schürz <minetoblend@gmail.com>
2024-02-18 21:42:48 +0100

    use environment for database config

--- packages/server/src/datasource.ts
+++ packages/server/src/datasource.ts
@@ -1,13 +1,14 @@
 import {DataSource, DataSourceOptions} from 'typeorm';
 import {isProduction} from "./utils/env";
+import * as process from "process";
 
 export const dbdatasource: DataSourceOptions = {
   type: 'mysql',
-  host: 'mysql',
-  port: 3306,
-  username: 'osucad',
-  password: 'test',
-  database: 'osucad',
+  host: process.env.MYSQL_HOST ?? 'mysql',
+  port: parseInt(process.env.MYSQL_PORT ?? '3306'),
+  username: process.env.MYSQL_USER ?? 'osucad',
+  password: process.env.MYSQL_PASSWORD ?? 'osucad',
+  database: process.env.MYSQL_DATABASE ?? 'osucad',
   synchronize: false,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/tasks/migrations/*.js'],
