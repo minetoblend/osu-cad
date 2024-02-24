e9d405de3611b7332358747638e580b5038b0164
root <root@vmi632098.contaboserver.net>
2024-02-24 22:57:17 +0100

    fix incorrect constraint name in migration

--- packages/server/src/tasks/migrations/1708805723958-participant-properties.ts
+++ packages/server/src/tasks/migrations/1708805723958-participant-properties.ts
@@ -5,7 +5,7 @@ export class ParticipantProperties1708805723958 implements MigrationInterface {
     name = 'ParticipantProperties1708805723958'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`participant\` DROP FOREIGN KEY \`FK_b915e97dea27ffd1e40c8003b3b\``);
+        await queryRunner.query(`ALTER TABLE \`participant\` DROP FOREIGN KEY \`FK_b054509707208f3971234785139\``);
         await queryRunner.query(`DROP INDEX \`FK_b054509707208f3971234785139\` ON \`participant\``);
         await queryRunner.query(`ALTER TABLE \`participant\` ADD \`username\` varchar(255) NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`participant\` CHANGE \`userId\` \`userId\` int NOT NULL`);
@@ -19,7 +19,7 @@ export class ParticipantProperties1708805723958 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`participant\` CHANGE \`userId\` \`userId\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`participant\` DROP COLUMN \`username\``);
         await queryRunner.query(`CREATE INDEX \`FK_b054509707208f3971234785139\` ON \`participant\` (\`beatmapId\`)`);
-        await queryRunner.query(`ALTER TABLE \`participant\` ADD CONSTRAINT \`FK_b915e97dea27ffd1e40c8003b3b\` FOREIGN KEY (\`userId\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`participant\` ADD CONSTRAINT \`FK_b054509707208f3971234785139\` FOREIGN KEY (\`userId\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
