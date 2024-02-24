a03de4e46ecd0771f8cd65d271db0f5fc131736c
Marvin Schürz <minetoblend@gmail.com>
2024-02-24 03:33:42 +0100

    Load sprites from sprite sheet instead of individually.

--- packages/client/src/editor/textures.ts
+++ packages/client/src/editor/textures.ts
@@ -1,93 +1,10 @@
 import { Assets, AssetsManifest } from 'pixi.js';
 import { skinBundle } from './assets/skin.ts';
-import { withBasePath } from './assets/basePath.ts';
+import spriteSheetUrl from '@/assets/spritesheet.json?url';
+import spriteSheetPngUrl from '@/assets/spritesheet.png?url';
 
 const manifest: AssetsManifest = {
-  bundles: [
-    skinBundle('skin'),
-    withBasePath('ui', {
-      name: 'ui',
-      assets: [
-        {
-          alias: 'timeline-tick',
-          src: 'timeline-tick.png',
-        },
-        {
-          alias: 'sliderpath-handle',
-          src: 'sliderpath-handle.png',
-        },
-        {
-          alias: 'play',
-          src: 'play.png',
-        },
-        {
-          alias: 'pause',
-          src: 'pause.png',
-        },
-        {
-          alias: 'hitsound-layer-toggle',
-          src: 'hitsound-layer-toggle.png',
-        },
-        {
-          alias: 'hitsample',
-          src: 'hitsample.png',
-        },
-        {
-          alias: 'hitsample-outline',
-          src: 'hitsample-outline.png',
-        },
-        {
-          alias: 'timeline-marker',
-          src: 'timeline-marker.png',
-        },
-      ],
-    }),
-    withBasePath('icons', {
-      name: 'icons',
-      assets: [
-        {
-          alias: 'icon-select',
-          src: 'select.png',
-        },
-        {
-          alias: 'icon-circle',
-          src: 'circle.png',
-        },
-        {
-          alias: 'icon-slider',
-          src: 'slider.png',
-        },
-        {
-          alias: 'icon-spinner',
-          src: 'spinner.png',
-        },
-        {
-          alias: 'icon-undo',
-          src: 'undo.png',
-        },
-        {
-          alias: 'icon-redo',
-          src: 'redo.png',
-        },
-        {
-          alias: 'icon-size-ew',
-          src: 'size-ew.png',
-        },
-        {
-          alias: 'icon-size-ns',
-          src: 'size-ns.png',
-        },
-        {
-          alias: 'icon-reverse',
-          src: 'reverse.png',
-        },
-        {
-          alias: 'icon-cog',
-          src: 'cog.png',
-        },
-      ],
-    }),
-  ],
+  bundles: [skinBundle('skin')],
 };
 
 export async function createEditorTextures() {
@@ -96,5 +13,15 @@ export async function createEditorTextures() {
     basePath: '/assets',
     manifest,
   });
-  await Assets.loadBundle(['ui', 'osu-skin', 'icons']);
+  await Assets.loadBundle(['osu-skin']);
+
+  const spritesheet = await Assets.load({
+    src: spriteSheetUrl,
+    data: {
+      texture: await Assets.load(spriteSheetPngUrl),
+    },
+  });
+  for (const [name, texture] of Object.entries(spritesheet.textures)) {
+    Assets.cache.set(name, texture);
+  }
 }
