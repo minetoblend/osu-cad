d8a32c86261b6f31ecd0813d9e65e604fe124e83
Marvin Schürz <minetoblend@gmail.com>
2024-02-25 23:58:08 +0100

    Remove empty service

--- packages/server/src/admin/admin.module.ts
+++ packages/server/src/admin/admin.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
 import { UserModule } from '../users/user.module';
 import { EditorModule } from '../editor/editor.module';
 
 @Module({
   imports: [UserModule, EditorModule],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [],
 })
 export class AdminModule {}
