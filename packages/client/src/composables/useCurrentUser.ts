1cc12028600fb3eee3cc3b4b43978b08bc52cf87
Marvin Schürz <minetoblend@gmail.com>
2024-02-27 17:21:56 +0100

    Send current user info as part of html content

--- packages/client/src/composables/useCurrentUser.ts
+++ packages/client/src/composables/useCurrentUser.ts
@@ -2,6 +2,18 @@ import { UserInfo } from '@osucad/common';
 import axios from 'axios';
 
 export const useCurrentUser = createGlobalState(() => {
+  const element: HTMLElement | null = document.getElementById('user-json');
+
+  if (element) {
+    const user = JSON.parse(element.textContent ?? 'null') as UserInfo;
+
+    return {
+      user: ref(user),
+      isLoading: ref(false),
+      isAdmin: computed(() => user.isAdmin),
+    };
+  }
+
   const { state: user, isLoading } = useAsyncState(async () => {
     const response = await axios.get<UserInfo>('/api/users/me');
     return response.data;
