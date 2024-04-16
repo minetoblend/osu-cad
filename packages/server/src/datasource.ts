4eabc5d3525f4c173e0b723d8b2020f4e7dbada2
Marvin <m.schuerz@hautzy.com>
2024-04-16 15:45:57 +0200

    Add ssl to mysql config

--- packages/server/src/datasource.ts
+++ packages/server/src/datasource.ts
@@ -15,6 +15,7 @@ export const dbdatasource: DataSourceOptions = {
   username: process.env.MYSQL_USER ?? 'osucad',
   password: process.env.MYSQL_PASSWORD ?? 'osucad',
   database: process.env.MYSQL_DATABASE ?? 'osucad',
+  ssl: process.env.MYSQL_SSL === 'true',
   synchronize: false,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/tasks/migrations/*.js'],
