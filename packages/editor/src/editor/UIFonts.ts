96bda80756f6ea4954d8a203905358246579e1a8
Marvin Schürz <m.schuerz@hautzy.com>
2024-06-30 01:37:47 +0200

    Move fonts to public dir

--- packages/editor/src/editor/UIFonts.ts
+++ packages/editor/src/editor/UIFonts.ts
@@ -2,20 +2,20 @@ import { FontDefinition } from 'osucad-framework';
 
 export class UIFonts {
   readonly nunitoSans = new FontDefinition(
-    new URL('../assets/fonts/nunito-sans-400.fnt', import.meta.url).href,
-    new URL('../assets/fonts/nunito-sans-400.png', import.meta.url).href,
+    '/assets/fonts/nunito-sans-400.fnt',
+    '/assets/fonts/nunito-sans-400.png',
   );
   readonly nunitoSans500 = new FontDefinition(
-    new URL('../assets/fonts/nunito-sans-500.fnt', import.meta.url).href,
-    new URL('../assets/fonts/nunito-sans-500.png', import.meta.url).href,
+    '/assets/fonts/nunito-sans-500.fnt',
+    '/assets/fonts/nunito-sans-500.png',
   );
   readonly nunitoSans600 = new FontDefinition(
-    new URL('../assets/fonts/nunito-sans-600.fnt', import.meta.url).href,
-    new URL('../assets/fonts/nunito-sans-600.png', import.meta.url).href,
+    '/assets/fonts/nunito-sans-600.fnt',
+    '/assets/fonts/nunito-sans-600.png',
   );
   readonly nunitoSans700 = new FontDefinition(
-    new URL('../assets/fonts/nunito-sans-700.fnt', import.meta.url).href,
-    new URL('../assets/fonts/nunito-sans-700.png', import.meta.url).href,
+    '/assets/fonts/nunito-sans-700.fnt',
+    '/assets/fonts/nunito-sans-700.png',
   );
 
   async load(): Promise<void> {
