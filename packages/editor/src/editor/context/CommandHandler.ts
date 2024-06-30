eb67f4af9fff8d15f2435663f264a049f80568d7
Marvin Schürz <m.schuerz@hautzy.com>
2024-07-01 01:41:06 +0200

    Add canUndo & canRedo bindables to CommandHandler

--- packages/editor/src/editor/context/CommandHandler.ts
+++ packages/editor/src/editor/context/CommandHandler.ts
@@ -1,9 +1,14 @@
 import { Beatmap } from '@osucad/common';
 import { EditorContext } from './EditorContext';
+import { Bindable } from 'osucad-framework';
 
 export class CommandHandler {
   constructor(
     readonly context: EditorContext,
     readonly beatmap: Beatmap,
   ) {}
+
+  readonly canUndo = new Bindable(false);
+
+  readonly canRedo = new Bindable(false);
 }
