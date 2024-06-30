bc7b3f0922dad4c4d6dcd262688ea620f247c13d
Marvin <m.schuerz@hautzy.com>
2024-06-30 22:23:32 +0200

    Use correct path for loading fonts

--- packages/editor/src/editor/UIFonts.ts
+++ packages/editor/src/editor/UIFonts.ts
@@ -2,19 +2,19 @@ import { FontDefinition } from 'osucad-framework';
 
 export class UIFonts {
   readonly nunitoSans = new FontDefinition(
-    '/assets/fonts/nunito-sans-400.fnt',
+    '/assets/fonts/nunito-sans-latin-400-normal.fnt',
     '/assets/fonts/nunito-sans-400.png',
   );
   readonly nunitoSans500 = new FontDefinition(
-    '/assets/fonts/nunito-sans-500.fnt',
+    '/assets/fonts/nunito-sans-latin-500-normal.fnt',
     '/assets/fonts/nunito-sans-500.png',
   );
   readonly nunitoSans600 = new FontDefinition(
-    '/assets/fonts/nunito-sans-600.fnt',
+    '/assets/fonts/nunito-sans-latin-600-normal.fnt',
     '/assets/fonts/nunito-sans-600.png',
   );
   readonly nunitoSans700 = new FontDefinition(
-    '/assets/fonts/nunito-sans-700.fnt',
+    '/assets/fonts/nunito-sans-latin-700-normal.fnt',
     '/assets/fonts/nunito-sans-700.png',
   );
 
