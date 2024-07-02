1ac350357e4556306dc38b099daedbbf65cae7c3
Marvin <m.schuerz@hautzy.com>
2024-07-02 18:52:55 +0200

    Implement basic hitcircle tool

--- packages/editor/src/editor/screens/compose/tools/ComposeTool.ts
+++ packages/editor/src/editor/screens/compose/tools/ComposeTool.ts
@@ -1,9 +1,37 @@
-import { Axes, Container } from 'osucad-framework';
+import { Beatmap, HitObjectManager } from '@osucad/common';
+import { Axes, InputManager, resolved } from 'osucad-framework';
+import { CommandContainer } from '../../../CommandContainer';
+import { EditorClock } from '../../../EditorClock';
 
-export abstract class ComposeTool extends Container {
+export abstract class ComposeTool extends CommandContainer {
   protected constructor() {
     super({
       relativeSizeAxes: Axes.Both,
     });
   }
+
+  #inputManager: InputManager | null = null;
+
+  get inputManager(): InputManager {
+    if (!this.#inputManager) {
+      this.#inputManager ??= this.getContainingInputManager();
+
+      if (!this.#inputManager) throw new Error('InputManager not found');
+    }
+
+    return this.#inputManager;
+  }
+
+  get mousePosition() {
+    return this.toLocalSpace(this.inputManager.currentState.mouse.position);
+  }
+
+  @resolved(Beatmap)
+  protected readonly beatmap!: Beatmap;
+
+  @resolved(HitObjectManager)
+  protected readonly hitObjects!: HitObjectManager;
+
+  @resolved(EditorClock)
+  protected readonly editorClock!: EditorClock;
 }
--- packages/editor/src/editor/screens/compose/tools/HitCircleTool.ts
+++ packages/editor/src/editor/screens/compose/tools/HitCircleTool.ts
@@ -1,7 +1,83 @@
+import {
+  CreateHitObjectCommand,
+  DeleteHitObjectCommand,
+  HitCircle,
+} from '@osucad/common';
 import { ComposeTool } from './ComposeTool';
+import {
+  MouseDownEvent,
+  MouseMoveEvent,
+  dependencyLoader,
+} from 'osucad-framework';
 
 export class HitCircleTool extends ComposeTool {
   constructor() {
     super();
+  }
+
+  #previewCircle!: HitCircle;
+
+  @dependencyLoader()
+  load() {
+    this.editorClock.currentTimeBindable.addOnChangeListener(() => {
+      if (this.#previewCircle) {
+        this.#previewCircle.startTime = this.editorClock.currentTime;
+      }
+    });
+  }
+
+  update(): void {
+    super.update();
+
+    if (!this.#previewCircle) {
+      this.#beginPlacing();
+    }
+  }
+
+  #beginPlacing() {
+    const circle = new HitCircle();
+    circle.position = this.mousePosition;
+    circle.startTime = this.editorClock.currentTime;
+
+    this.hitObjects.add(circle);
+
+    this.#previewCircle = circle;
+  }
+
+  onMouseMove(e: MouseMoveEvent): boolean {
+    this.#previewCircle.position = e.mousePosition;
+
+    return true;
+  }
+
+  onMouseDown(e: MouseDownEvent): boolean {
+    if (e.button === 0) {
+      const circle = this.#previewCircle;
+      const placementTime = this.editorClock.currentTime;
+
+      this.hitObjects.remove(circle);
+
+      let existing = this.hitObjects.getAtTime(placementTime);
+
+      while (existing) {
+        this.submit(new DeleteHitObjectCommand(existing), false);
+        existing = this.hitObjects.getAtTime(placementTime);
+      }
+
+      circle.startTime = placementTime;
+
+      this.submit(new CreateHitObjectCommand(circle));
+
+      this.#beginPlacing();
+
+      return true;
+    }
+    return false;
+  }
+
+  dispose(): boolean {
+    this.hitObjects.remove(this.#previewCircle);
+
+    return true;
   }
 }
