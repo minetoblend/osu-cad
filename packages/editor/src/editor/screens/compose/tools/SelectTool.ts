c1da516fa8dbf463cebd3841ad0bfb9669f1b5de
Marvin <m.schuerz@hautzy.com>
2024-07-03 18:45:37 +0200

    Start select box interaction on drag start instead of mouse down

--- packages/editor/src/editor/screens/compose/tools/SelectTool.ts
+++ packages/editor/src/editor/screens/compose/tools/SelectTool.ts
@@ -1,5 +1,5 @@
 import { ComposeTool } from './ComposeTool';
-import { MouseButton, MouseDownEvent } from 'osucad-framework';
+import { DragStartEvent, MouseButton, MouseDownEvent } from 'osucad-framework';
 import { SelectBoxInteraction } from './interactions/SelectBoxInteraction';
 
 export class SelectTool extends ComposeTool {
@@ -7,9 +7,15 @@ export class SelectTool extends ComposeTool {
     super();
   }
 
-  override onMouseDown(e: MouseDownEvent): boolean {
+  onDragStart(e: DragStartEvent): boolean {
     if (e.button === MouseButton.Left) {
-      this.beginInteraction(new SelectBoxInteraction(e.mousePosition));
+      this.beginInteraction(
+        new SelectBoxInteraction(
+          this.toLocalSpace(
+            e.screenSpaceMouseDownPosition ?? e.screenSpaceMousePosition,
+          ),
+        ),
+      );
 
       return true;
     }
