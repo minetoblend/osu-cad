806807c3fe768e42751ef2fc9d7440c4ea071370
Marvin Schürz <minetoblend@gmail.com>
2024-07-07 00:29:10 +0200

    Add slider reverse arrows

--- packages/editor/src/editor/hitobjects/DrawableSlider.ts
+++ packages/editor/src/editor/hitobjects/DrawableSlider.ts
@@ -1,12 +1,13 @@
 import { Slider } from '@osucad/common';
 import { DrawableHitObject } from './DrawableHitObject';
-import { Anchor, Vec2 } from 'osucad-framework';
+import { Anchor, Container, Vec2 } from 'osucad-framework';
 import { CirclePiece } from './CirclePiece';
 import { ApproachCircle } from './ApproachCircle';
 import { DrawableSliderBody } from './DrawableSliderBody';
 import { DrawableComboNumber } from './DrawableComboNumber';
 import { animate } from '../../utils/animate';
 import { DrawableSliderBall } from './DrawableSliderBall';
+import { ReverseArrow } from './ReverseArrow';
 
 export class DrawableSlider extends DrawableHitObject<Slider> {
   constructor(public slider: Slider) {
@@ -20,11 +21,13 @@ export class DrawableSlider extends DrawableHitObject<Slider> {
   sliderBody!: DrawableSliderBody;
   comboNumber!: DrawableComboNumber;
   sliderBall!: DrawableSliderBall;
+  reverseArrows!: Container;
 
   override load() {
     this.addAll(
       (this.sliderBody = new DrawableSliderBody(this.hitObject)),
       (this.tailCircle = new CirclePiece()),
+      (this.reverseArrows = new Container()),
       (this.headCircle = new CirclePiece()),
       (this.approachCircle = new ApproachCircle()),
       (this.sliderBall = new DrawableSliderBall()),
@@ -50,11 +53,40 @@ export class DrawableSlider extends DrawableHitObject<Slider> {
       Math.min(150, this.hitObject.spanDuration / 2);
     this.tailCircle.timeFadeIn = this.hitObject.timeFadeIn;
     this.tailCircle.comboColor = this.comboColor;
-    (this.tailCircle.position = Vec2.scale(
+    this.tailCircle.position = Vec2.scale(
       Vec2.sub(this.hitObject.endPosition, this.hitObject.position),
       1 / this.hitObject.scale,
-    )),
-      (this.sliderBody.scale = new Vec2(1 / this.hitObject.scale));
+    );
+
+    this.reverseArrows.clear();
+    for (let i = 0; i < this.hitObject.repeats; i++) {
+      const time = this.hitObject.startTime + this.hitObject.spanDuration * i;
+      const circle = new CirclePiece();
+      circle.startTime = time;
+      circle.timePreempt = this.hitObject.timePreempt;
+      circle.timeFadeIn = this.hitObject.timeFadeIn;
+      circle.comboColor = this.comboColor;
+
+      const angle =
+        i % 2 == 0 ? this.hitObject.endAngle : this.hitObject.startAngle;
+
+      const reverseArrow = new ReverseArrow();
+      reverseArrow.rotation = angle + Math.PI;
+      reverseArrow.startTime = time;
+      reverseArrow.timePreempt = this.hitObject.timePreempt;
+
+      this.reverseArrows.addAll(circle, reverseArrow);
+      if (i % 2 === 0) {
+        const position = Vec2.scale(
+          this.hitObject.path.endPosition,
+          1 / this.hitObject.scale,
+        );
+        reverseArrow.position = position;
+        circle.position = position;
+      }
+    }
+
+    this.sliderBody.scale = new Vec2(1 / this.hitObject.scale);
     this.sliderBody.setup();
 
     this.approachCircle.comboColor = this.comboColor;
---
+++ packages/editor/src/editor/hitobjects/ReverseArrow.ts
@@ -0,0 +1,59 @@
+import {
+  Anchor,
+  CompositeDrawable,
+  dependencyLoader,
+  DrawableSprite,
+  resolved,
+} from 'osucad-framework';
+import { animate } from '../../utils/animate';
+import { EditorClock } from '../EditorClock';
+import { Skin } from '../../skins/Skin';
+
+export class ReverseArrow extends CompositeDrawable {
+  constructor() {
+    super();
+  }
+
+  @resolved(Skin)
+  skin!: Skin;
+
+  @dependencyLoader()
+  load() {
+    this.addInternal(
+      new DrawableSprite({
+        texture: this.skin.reverseArrow,
+        origin: Anchor.Center,
+      }),
+    );
+  }
+
+  get alwaysPresent(): boolean {
+    return true;
+  }
+
+  startTime = 0;
+  timePreempt = 0;
+
+  @resolved(EditorClock)
+  editorClock!: EditorClock;
+
+  update() {
+    super.update();
+
+    const time = this.time.current - this.startTime;
+
+    if (time < 0) {
+      this.alpha = animate(
+        time,
+        -this.timePreempt,
+        -this.timePreempt + 150,
+        0,
+        1,
+      );
+      this.scale = 1.2 - this.editorClock.beatProgress * 0.2;
+    } else {
+      this.alpha = animate(time, 0, 150, 1, 0);
+      this.scale = animate(time, 0, 150, 1, 2);
+    }
+  }
+}
