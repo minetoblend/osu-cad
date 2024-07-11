04ddf92eec9611f52c5b3f8667f8dabe58df7b75
Marvin <m.schuerz@hautzy.com>
2024-07-11 23:53:08 +0200

    make sampleType readonly

--- packages/editor/src/editor/screens/compose/ComposeAdditionButton.ts
+++ packages/editor/src/editor/screens/compose/ComposeAdditionButton.ts
@@ -3,7 +3,10 @@ import { ComposeToolbarButton } from './ComposeToolbarButton';
 import { SampleType } from '@osucad/common';
 
 export class ComposeAdditionButton extends ComposeToolbarButton {
-  constructor(icon: Texture, type: SampleType) {
+  constructor(
+    icon: Texture,
+    readonly sampleType: SampleType,
+  ) {
     super(icon);
   }
 }
