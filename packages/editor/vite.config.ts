b7fe27b420890cdc2de6f0e60b20d82c13848815
Marvin <m.schuerz@hautzy.com>
2024-07-12 03:30:43 +0200

    create vite plugin to load textures

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,6 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
+
+const texturePlugin: Plugin = {
+  name: 'pixi textures',
+  enforce: 'pre',
+  load(id) {
+    if(!id.endsWith('?texture')) return null;
+
+    const path = id.split('?')[0]
+
+    return `
+    import { Assets } from 'pixi.js'
+    import url from ${JSON.stringify(path)}
+
+    export default await Assets.load(url) 
+    `
+  }
+}
 
 export default defineConfig({
+  plugins: [texturePlugin],
   worker: {
     format: 'es'
   },
