03bd814c4b4e2671b977ba3b75b21f0681972af7
Marvin Schürz <minetoblend@gmail.com>
2024-07-16 22:19:09 +0200

    Add user join/leave sound samples

--- packages/editor/src/UISamples.ts
+++ packages/editor/src/UISamples.ts
@@ -3,6 +3,8 @@ import type { AudioChannel, AudioManager, Sample } from 'osucad-framework';
 import toolHover from './assets/samples/tool-hover.ogg';
 import toolSelect from './assets/samples/tool-select.ogg';
 import sliderDrag from './assets/samples/slider-drag.ogg';
+import userJoined from './assets/samples/user-joined.ogg';
+import userLeft from './assets/samples/user-left.ogg';
 
 export class UISamples {
   constructor(
@@ -13,12 +15,16 @@ export class UISamples {
   toolHover!: Sample;
   toolSelect!: Sample;
   sliderDrag!: Sample;
+  userJoined!: Sample;
+  userLeft!: Sample;
 
   async load() {
     await Promise.all([
       this.#loadSample('toolHover', toolHover),
       this.#loadSample('toolSelect', toolSelect),
       this.#loadSample('sliderDrag', sliderDrag),
+      this.#loadSample('userJoined', userJoined),
+      this.#loadSample('userLeft', userLeft),
     ]);
   }
 
