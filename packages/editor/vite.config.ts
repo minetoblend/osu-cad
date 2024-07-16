9f4ec5e52b3905761bd63367d77fff154e2d7648
Marvin Schürz <minetoblend@gmail.com>
2024-07-16 22:17:42 +0200

    Remove fonts plugin

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,47 +1,9 @@
-import { defineConfig, type Plugin } from 'vite';
-import PixiAssets from 'unplugin-pixi-assets/vite'
-import { resolve, join } from 'path'
-import * as fs from 'fs/promises'
-import Dts from 'vite-plugin-dts'
-
-
-const fontsPlugin: Plugin = {
-  name: 'fonts',
-  enforce: 'pre',
- async load(id) {
-    if (id.endsWith('.fnt?bmFont')) {
-      const path = id.split('?')[0]
-
-      const texturePath = (path.split('.').slice(0, -1)).join('.') + '.png'
-
-      console.log(texturePath)
-
-      this.emitFile({
-        type: 'asset',
-        name: texturePath.split('/').pop(),
-        needsCodeReference: false,
-        source: await fs.readFile(resolve(process.cwd(), texturePath))
-      })
-
-      return `
-      import { FontDefinition } from 'osucad-framework'
-
-      const font = new FontDefinition(
-        new URL(${JSON.stringify(path)}, import.meta.url).href,
-        new URL(${JSON.stringify(texturePath)}, import.meta.url).href,
-      )
-
-      await font.load()
-
-      export default font
-      `
-    }
-  }
-}
+import { join } from 'node:path';
+import { defineConfig } from 'vite';
+import PixiAssets from 'unplugin-pixi-assets/vite';
 
 export default defineConfig({
   plugins: [
-    fontsPlugin,
     PixiAssets({
       assetsFolder: [
         {
@@ -50,44 +12,35 @@ export default defineConfig({
             prefix: 'icon:',
             dotNotation: true,
             stripExtensions: true,
-          }
-        }
+          },
+        },
       ],
       textures: {
         defaultOptions: {
-          autoGenerateMipmaps: true
-        }
-      }
+          autoGenerateMipmaps: true,
+        },
+      },
     }),
-    Dts(),
   ],
+  base: '/edit/',
   resolve: {
     alias: {
-      '@icons': join(__dirname, 'src/assets/icons')
-    }
+      '@icons': join(__dirname, 'src/assets/icons'),
+    },
   },
   worker: {
-    format: 'es'
+    format: 'es',
   },
   build: {
-    target: "esnext",
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'osucad-editor',
-      formats: ['es'],
-      fileName: 'index',
-
-    },
-
+    target: 'esnext',
     rollupOptions: {
-
       output: {
         assetFileNames(chunkInfo) {
-          if(chunkInfo.name?.includes('nunito-sans'))
-            return `assets/[name].[ext]`
-          return  `assets/[name]-[hash].[ext]`;
+          if (chunkInfo.name?.includes('nunito-sans'))
+            return `assets/[name].[ext]`;
+          return `assets/[name]-[hash].[ext]`;
         },
-      }
-    }
-  }
+      },
+    },
+  },
 });
