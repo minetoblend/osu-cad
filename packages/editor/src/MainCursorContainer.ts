ddca4ecaa4c0b8a019b3dab69e87b97cc872bd84
Marvin Schürz <minetoblend@gmail.com>
2024-07-22 07:39:03 +0200

    Add shadow to cursor

--- packages/editor/src/MainCursorContainer.ts
+++ packages/editor/src/MainCursorContainer.ts
@@ -19,11 +19,19 @@ export class MainCursorContainer extends CursorContainer {
 }
 
 class Cursor extends CompositeDrawable {
+  #shadow!: DrawableSprite;
   #sprite!: DrawableSprite;
 
   @dependencyLoader()
   load() {
-    this.addInternal(
+    this.addAllInternal(
+      (this.#shadow = new DrawableSprite({
+        texture: useAsset('icon:select'),
+        x: -4,
+        y: -1,
+        color: 0x000000,
+        alpha: 0.2,
+      })),
       (this.#sprite = new DrawableSprite({
         texture: useAsset('icon:select'),
         x: -4,
@@ -43,6 +51,16 @@ class Cursor extends CompositeDrawable {
         duration: 1.0,
         ease: 'power4.out',
       });
+
+      gsap.killTweensOf(this.#shadow);
+      gsap.to(this.#shadow, {
+        scaleX: 0.8,
+        scaleY: 0.8,
+        x: -5,
+        y: -2,
+        duration: 1.0,
+        ease: 'power4.out',
+      });
     }
     return false;
   }
@@ -55,6 +73,16 @@ class Cursor extends CompositeDrawable {
         scaleY: 1,
         x: -4,
         y: -3,
+        duration: 0.2,
+        ease: 'back.out',
+      });
+
+      gsap.killTweensOf(this.#shadow);
+      gsap.to(this.#shadow, {
+        scaleX: 1,
+        scaleY: 1,
+        x: -4,
+        y: -1,
         duration: 0.2,
         ease: 'back.out',
       });
