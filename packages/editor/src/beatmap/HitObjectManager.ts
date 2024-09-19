3217f7b505b42575b2e012c827aa254a343f8e7a
Marvin Schürz <minetoblend@gmail.com>
2024-09-19 23:27:34 +0200

    Remove HitObjectManager.ts

--- packages/editor/src/beatmap/HitObjectManager.ts
+++
@@ -1,7 +0,0 @@
-import type { SortedList } from 'osucad-framework';
-import type { HitObject } from '@osucad/common';
-
-export class HitObjectManager {
-  constructor(readonly hitObjects: SortedList<HitObject>) {
-  }
-}
