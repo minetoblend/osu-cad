00e3b2bdd57929888084b9e6e0bae097229e24d0
Marvin Schürz <minetoblend@gmail.com>
2024-09-20 00:16:13 +0200

    Add ipc handler for saving beatmaps

--- packages/electron-app/src/main/ElectronEnvironment.ts
+++ packages/electron-app/src/main/ElectronEnvironment.ts
@@ -1,8 +1,9 @@
 import { ipcMain } from 'electron';
-import { readFile, readdir } from 'node:fs/promises';
+import { readFile, writeFile, readdir, mkdir, copyFile } from 'node:fs/promises';
 import OsuDBParser, { OsuBeatmap } from 'osu-db-parser';
 import { OsuStableInfo } from './loadOsuStableInfo.ts';
 import path from 'node:path';
+import log from 'electron-log/main'
 
 export async function setupEnvironment(osuPaths: OsuStableInfo | null) {
   ipcMain.on('stableDetected', (event) => {
@@ -11,6 +12,10 @@ export async function setupEnvironment(osuPaths: OsuStableInfo | null) {
 
   ipcMain.on('argv', (event) => {
     event.returnValue = process.argv;
+  });
+
+  ipcMain.on('osuPaths', (event) => {
+    event.returnValue = osuPaths;
   });
 
   ipcMain.handle('loadBeatmaps', async () => {
@@ -28,6 +33,44 @@ export async function setupEnvironment(osuPaths: OsuStableInfo | null) {
 
     return await loadSkins(osuPaths);
   });
+
+  ipcMain.handle('saveBeatmap', async (_, beatmap: string, filename: string, content: string) => {
+    if (!osuPaths?.songsPath)
+      return false;
+
+    const beatmapDirectory = path.join(osuPaths.songsPath, beatmap);
+
+    const backupPath = path.join(beatmapDirectory, '.osucad/backups')
+
+    try {
+      await mkdir(backupPath, {
+        recursive: true
+      })
+
+      log.info(
+        'Creating backup for ',
+        path.resolve(beatmapDirectory, filename),
+        ' in ',
+        path.resolve(backupPath, `${new Date().toString()}_${filename}`)
+      )
+
+      const originalFileContents = await readFile(path.resolve(beatmapDirectory, filename), 'utf-8')
+
+      await writeFile(
+        path.resolve(backupPath, `${new Date().getTime()}_${filename}.backup`),
+        originalFileContents,
+      )
+
+      await writeFile(path.resolve(beatmapDirectory, filename), content)
+
+      log.info(`Successfully saved ${filename}`)
+
+      return true
+    } catch (e) {
+      log.error(e)
+      return false
+    }
+  })
 }
 
 async function loadSkins(osuPaths: OsuStableInfo): Promise<ElectronSkinInfo[]> {
