a97dbb8e77d184fc80e537415359eaa5d59a43f6
Marvin Schürz <minetoblend@gmail.com>
2024-09-20 00:00:12 +0200

    Using bound copy of newCombo for combo toggle button

--- packages/editor/src/editor/screens/compose/ComposeComboToggleButton.ts
+++ packages/editor/src/editor/screens/compose/ComposeComboToggleButton.ts
@@ -9,11 +9,49 @@ export class ComposeComboToggleButton extends ComposeToggleButton {
     super(OsucadIcons.get('new-combo'));
   }
 
-  @resolved(NEW_COMBO)
-  newCombo!: BindableBoolean;
+  newCombo = new BindableBoolean();
+
+  @resolved(NEW_COMBO_APPLIED)
+  newComboApplied!: Action<boolean>;
 
   @dependencyLoader()
   load() {
+    this.newCombo.bindTo(this.dependencies.resolve(NEW_COMBO));
+
     this.active.bindTo(this.newCombo);
+  }
+
+  readonly isKeyBindingHandler = true;
+
+  canHandleKeyBinding(binding: EditorAction): boolean {
+    return binding instanceof EditorAction;
+  }
+
+  onKeyBindingPressed(event: KeyBindingPressEvent<EditorAction>) {
+    if (event.pressed === EditorAction.ToggleNewCombo) {
+      this.armed = true;
+      this.triggerAction();
+      return true;
+    }
+
+    return false;
+  }
+
+  onKeyBindingReleased(event: KeyBindingReleaseEvent<EditorAction>) {
+    if (event.pressed === EditorAction.ToggleNewCombo) {
+      this.armed = false;
+    }
+  }
+
+  protected onActivate() {
+    super.onActivate();
+
+    this.newComboApplied.emit(true);
+  }
+
+  protected onDeactivate() {
+    super.onDeactivate();
+
+    this.newComboApplied.emit(false);
   }
 }
