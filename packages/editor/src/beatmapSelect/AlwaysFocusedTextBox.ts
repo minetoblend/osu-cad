efef8642ef290d3aca7cc4c1476ca13d597a0a1a
Marvin Schürz <minetoblend@gmail.com>
2024-09-25 23:43:36 +0200

    Disable arrow left/right keys in beatmap select textbox

--- packages/editor/src/beatmapSelect/AlwaysFocusedTextBox.ts
+++ packages/editor/src/beatmapSelect/AlwaysFocusedTextBox.ts
@@ -1,3 +1,5 @@
+import type { KeyBindingPressEvent } from 'osucad-framework';
+import { PlatformAction } from 'osucad-framework';
 import { TextBox } from '../userInterface/TextBox';
 
 export class AlwaysFocusedTextBox extends TextBox {
@@ -11,5 +13,12 @@ export class AlwaysFocusedTextBox extends TextBox {
     super.loadComplete();
 
     this.getContainingFocusManager()?.changeFocus(this);
+  }
+
+  onKeyBindingPressed(e: KeyBindingPressEvent<PlatformAction>): boolean {
+    if (e.pressed === PlatformAction.MoveForwardChar || e.pressed === PlatformAction.MoveBackwardChar)
+      return false;
+
+    return super.onKeyBindingPressed(e);
   }
 }
