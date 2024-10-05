33d8678a7a8117698a883b3509ab920d232fc694
Marvin Schürz <minetoblend@gmail.com>
2024-10-05 19:24:04 +0200

    Allow switching focus to other components in AlwaysFocusedTextBox

--- packages/editor/src/beatmapSelect/AlwaysFocusedTextBox.ts
+++ packages/editor/src/beatmapSelect/AlwaysFocusedTextBox.ts
@@ -3,22 +3,14 @@ import { PlatformAction } from 'osucad-framework';
 import { TextBox } from '../userInterface/TextBox';
 
 export class AlwaysFocusedTextBox extends TextBox {
-  override onFocusLost() {
-    super.onFocusLost();
-
-    this.schedule(() => this.getContainingFocusManager()?.changeFocus(this));
-  }
-
-  protected loadComplete() {
-    super.loadComplete();
-
-    this.getContainingFocusManager()?.changeFocus(this);
-  }
-
   onKeyBindingPressed(e: KeyBindingPressEvent<PlatformAction>): boolean {
     if (e.pressed === PlatformAction.MoveForwardChar || e.pressed === PlatformAction.MoveBackwardChar)
       return false;
 
     return super.onKeyBindingPressed(e);
+  }
+
+  get requestsFocus(): boolean {
+    return true;
   }
 }
