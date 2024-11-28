b9c6b06cc9e3ccc4da6706a18a3dacd04c37b25f
Marvin Schürz <minetoblend@gmail.com>
2024-11-28 07:43:44 +0100

    Add return types to all methods in encoder class

--- packages/serialization/src/encoder/Encoder.ts
+++ packages/serialization/src/encoder/Encoder.ts
@@ -2,75 +2,75 @@ import type { SerialDescriptor } from '../descriptor/SerialDescriptor';
 import type { SerializationStrategy } from '../Serializer';
 
 export interface Encoder {
-  encodeNotNullMark();
+  encodeNotNullMark(): void;
 
-  encodeNull();
+  encodeNull(): void;
 
-  encodeBoolean(value: boolean);
+  encodeBoolean(value: boolean): void;
 
-  encodeUint8(value: number);
+  encodeUint8(value: number): void;
 
-  encodeUint16(value: number);
+  encodeUint16(value: number): void;
 
-  encodeUint32(value: number);
+  encodeUint32(value: number): void;
 
-  encodeInt8(value: number);
+  encodeInt8(value: number): void;
 
-  encodeInt16(value: number);
+  encodeInt16(value: number): void;
 
-  encodeInt32(value: number);
+  encodeInt32(value: number): void;
 
-  encodeFloat32(value: number);
+  encodeFloat32(value: number): void;
 
-  encodeFloat64(value: number);
+  encodeFloat64(value: number): void;
 
-  encodeString(value: string);
+  encodeString(value: string): void;
 
   beginStructure(descriptor: SerialDescriptor): CompositeEncoder;
 
-  encodeStructure(descriptor: SerialDescriptor, fn: (encoder: CompositeEncoder) => void);
+  encodeStructure(descriptor: SerialDescriptor, fn: (encoder: CompositeEncoder) => void): void;
 
   beginCollection(descriptor: SerialDescriptor): CompositeEncoder;
 
-  encodeSerializableValue<T>(serializer: SerializationStrategy<T>, value: T);
+  encodeSerializableValue<T>(serializer: SerializationStrategy<T>, value: T): void;
 
-  encodeNullableSerializableValue<T>(serializer: SerializationStrategy<T>, value: T | null);
+  encodeNullableSerializableValue<T>(serializer: SerializationStrategy<T>, value: T | null): void;
 }
 
 export interface CompositeEncoder {
-  endStructure(descriptor: SerialDescriptor);
+  endStructure(descriptor: SerialDescriptor): void;
 
-  encodeBooleanElement(descriptor: SerialDescriptor, index: number, value: boolean);
+  encodeBooleanElement(descriptor: SerialDescriptor, index: number, value: boolean): void;
 
-  encodeUint8Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeUint8Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeUint16Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeUint16Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeUint32Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeUint32Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeInt8Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeInt8Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeInt16Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeInt16Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeInt32Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeInt32Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeFloat32Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeFloat32Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeFloat64Element(descriptor: SerialDescriptor, index: number, value: number);
+  encodeFloat64Element(descriptor: SerialDescriptor, index: number, value: number): void;
 
-  encodeStringElement(descriptor: SerialDescriptor, index: number, value: string);
+  encodeStringElement(descriptor: SerialDescriptor, index: number, value: string): void;
 
   encodeSerializableElement<T>(
     descriptor: SerialDescriptor,
     index: number,
     serializer: SerializationStrategy<T>,
     value: T
-  );
+  ): void;
 
   encodeNullableSerializableElement<T>(
     descriptor: SerialDescriptor,
     index: number,
     serializer: SerializationStrategy<T>,
     value: T | null
-  );
+  ): void;
 }
