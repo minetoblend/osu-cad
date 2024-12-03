bf572b5dbdbe43205224d4ad4e340d9ee345dcfb
Marvin Schürz <minetoblend@gmail.com>
2024-12-03 19:07:37 +0100

    Export Vec2Serializer from framework

--- packages/framework/src/math/index.ts
+++ packages/framework/src/math/index.ts
@@ -1,4 +1,4 @@
 export { lerp, lerpColor } from './lerp';
 export { Quad } from './Quad';
 export { Rectangle } from './Rectangle';
-export { type IVec2, Vec2 } from './Vec2';
+export { type IVec2, Vec2, Vec2Serializer } from './Vec2';
