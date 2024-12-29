805a38016884f30cd40a3c8c668b3bd3da36cef6
Marvin Schürz <m.schuerz@hautzy.com>
2024-12-30 00:22:25 +0100

    Fix interface functions being arrow functions

--- packages/framework/src/io/stores/IResourceStore.ts
+++ packages/framework/src/io/stores/IResourceStore.ts
@@ -1,13 +1,13 @@
 import type { IDisposable } from '../../types';
 
 export interface IResourceStore<T> extends IDisposable {
-  has: (name: string) => boolean;
+  has(name: string): boolean;
 
-  get: (name: string) => T | null;
+  get(name: string): T | null;
 
-  getAsync: (name: string) => Promise<T | null>;
+  getAsync(name: string): Promise<T | null>;
 
-  getAvailableResources: () => string[];
+  getAvailableResources(): string[];
 
-  canLoad: (name: string) => boolean;
+  canLoad(name: string): boolean;
 }
--- packages/framework/src/types/IDisposable.ts
+++ packages/framework/src/types/IDisposable.ts
@@ -1,3 +1,3 @@
 export interface IDisposable {
-  dispose: () => void;
+  dispose(): void;
 }
