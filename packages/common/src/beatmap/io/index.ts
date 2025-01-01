28fa2f0151b56c6d0e855656b08eca52043cf33c
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-01 07:32:54 +0100

    add exports to barrel file

--- packages/common/src/beatmap/io/index.ts
+++ packages/common/src/beatmap/io/index.ts
@@ -1,2 +1,7 @@
+export * from './FileStore';
+export * from './IFile';
+export * from './ResourceStoreBackedFileStore';
+export * from './SimpleFile';
 export * from './StableBeatmapEncoder';
 export * from './StableBeatmapParser';
+export * from './StaticFileStore';
