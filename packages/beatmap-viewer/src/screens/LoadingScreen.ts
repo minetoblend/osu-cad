f45bf22137fdb0fd47bad37dffa688fb6a6c1960
Marvin Schürz <minetoblend@gmail.com>
2025-01-02 20:00:37 +0100

    Load beatmap viewer async

--- packages/beatmap-viewer/src/screens/LoadingScreen.ts
+++ packages/beatmap-viewer/src/screens/LoadingScreen.ts
@@ -3,7 +3,6 @@ import type { ScreenTransitionEvent } from 'osucad-framework';
 import { OsucadScreen } from '@osucad/common';
 import { EditorLoadingSpinner } from '@osucad/editor/editor/EditorLoadingSpinner';
 import { Axes } from 'osucad-framework';
-import { BeatmapViewer } from './viewer/BeatmapViewer';
 
 export class LoadingScreen extends OsucadScreen {
   constructor(
@@ -23,7 +22,11 @@ export class LoadingScreen extends OsucadScreen {
 
     this.addInternal(this.#loadingSpinner);
 
-    this.loadBeatmap().then(beatmap => this.screenStack.push(new BeatmapViewer(beatmap)));
+    this.loadBeatmap().then(async (beatmap) => {
+      const { BeatmapViewer } = await import('../screens/viewer/BeatmapViewer');
+
+      this.screenStack.push(new BeatmapViewer(beatmap));
+    });
   }
 
   onResuming(e: ScreenTransitionEvent) {
