82ca260e583bcec9d5405771c41d1a723192d53f
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-06 06:14:31 +0100

    Detect beatmap difficulty from name

--- packages/common/src/verifier/VerifierBeatmap.ts
+++ packages/common/src/verifier/VerifierBeatmap.ts
@@ -8,6 +8,15 @@ import type { FileStore } from '../beatmap/io/FileStore';
 import type { ControlPointInfo } from '../controlPoints/ControlPointInfo';
 import type { HitObject } from '../hitObjects/HitObject';
 
+export enum DifficultyType {
+  Easy,
+  Normal,
+  Hard,
+  Insane,
+  Expert,
+  Ultra,
+}
+
 export class VerifierBeatmap<T extends HitObject = HitObject> implements IBeatmap<T> {
   constructor(
     readonly beatmap: Beatmap<T>,
@@ -21,6 +30,55 @@ export class VerifierBeatmap<T extends HitObject = HitObject> implements IBeatma
   get starRating(): number | null {
     this.calculateStarRating();
     return this.#starRating as number | null;
+  }
+
+  getDifficulty(considerName = false) {
+    let difficulty: DifficultyType | null = null;
+
+    const starRating = this.starRating;
+
+    if (starRating !== null) {
+      if (starRating < 2.0)
+        difficulty = DifficultyType.Easy;
+      else if (starRating < 2.7)
+        difficulty = DifficultyType.Normal;
+      else if (starRating < 4.0)
+        difficulty = DifficultyType.Hard;
+      else if (starRating < 5.3)
+        difficulty = DifficultyType.Insane;
+      else if (starRating < 6.5)
+        difficulty = DifficultyType.Expert;
+      else difficulty = DifficultyType.Ultra;
+    }
+
+    if (!considerName)
+      return difficulty;
+
+    return this.getDifficultyFromName() ?? difficulty;
+  }
+
+  #nameDiffPairs: [DifficultyType, string[]][] = [
+    [DifficultyType.Easy, ['Beginner', 'Easy', 'Novice']],
+    [DifficultyType.Normal, ['Basic', 'Normal', 'Medium', 'Intermediate']],
+    [DifficultyType.Hard, ['Advanced', 'Hard']],
+    [DifficultyType.Insane, ['Hyper', 'Insane']],
+    [DifficultyType.Expert, ['Expert', 'Extra', 'Extreme']],
+  ];
+
+  getDifficultyFromName() {
+    const name = this.metadata.difficultyName.toLowerCase();
+
+    const pairs = this.#nameDiffPairs.toReversed();
+
+    for (const [type, names] of pairs) {
+    // Allows difficulty names such as "Normal...!??" and ">{(__HARD;)}" to be detected,
+    // but still prevents "Normality" or similar inclusions.
+
+      if (names.some(it => new RegExp(String.raw`(^| )[!-@\\[-\`{{-~]*${it}[!-@\\[-\`{{-~]*( |$)`, 'i').test(name)))
+        return type;
+    }
+
+    return null;
   }
 
   calculateStarRating() {
