c6fd34682fb98060c9b74323899ad4efcab90d26
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-09 08:05:42 +0100

    Revert changes in web game class

--- packages/web/src/OsucadWebGame.ts
+++ packages/web/src/OsucadWebGame.ts
@@ -1,6 +1,6 @@
 import type { ReadonlyDependencyContainer } from 'osucad-framework';
-import { DummyEditorBeatmap, Editor, ISkinSource, OsucadGameBase, OsucadScreenStack, OsuRuleset, RulesetStore, SkinManager, Slider } from '@osucad/common';
-import { HoldNote, ManiaBeatmap, ManiaRuleset, Note, StageDefinition } from '@osucad/ruleset-mania';
+import { DummyEditorBeatmap, Editor, ISkinSource, OsucadGameBase, OsucadScreenStack, OsuRuleset, RulesetStore, SkinManager } from '@osucad/common';
+import { ManiaRuleset } from '@osucad/ruleset-mania';
 import { provide } from 'osucad-framework';
 
 export class OsucadWebGame extends OsucadGameBase {
@@ -13,7 +13,6 @@ export class OsucadWebGame extends OsucadGameBase {
 
     RulesetStore.register(OsuRuleset.rulesetInfo);
     RulesetStore.register(new ManiaRuleset().rulesetInfo);
-    // RulesetStore.register(new ManiaRuleset());
 
     this.addParallelLoad(this.loadComponentAsync(this.skinManager));
 
@@ -25,30 +24,6 @@ export class OsucadWebGame extends OsucadGameBase {
   protected loadComplete() {
     super.loadComplete();
 
-    const beatmap = new ManiaBeatmap(new StageDefinition(4));
-
-    const otherBeatmap = new DummyEditorBeatmap().beatmap;
-
-    beatmap.controlPoints = otherBeatmap.controlPoints;
-
-    beatmap.hitObjects.addAll(
-      otherBeatmap.hitObjects.items.map((it) => {
-        let note = new Note();
-
-        if (it instanceof Slider) {
-          note = new HoldNote();
-          (note as HoldNote).duration = it.duration;
-        }
-
-        note.startTime = it.startTime;
-        note.column = Math.floor(Math.random() * 4);
-
-        return note;
-      }),
-    );
-
-    const editorBeatmap = new DummyEditorBeatmap(beatmap as any);
-
-    this.#screenStack.push(new Editor(editorBeatmap));
+    this.#screenStack.push(new Editor(new DummyEditorBeatmap()));
   }
 }
