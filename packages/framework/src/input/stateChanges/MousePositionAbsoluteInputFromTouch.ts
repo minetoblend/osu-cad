573d6bce2f4d16071df2c4b17150189ad90e9eb1
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-09 17:09:52 +0100

    Fix `MousePositionAbsoluteInputFromTouch` not implementing `ISourcedFromTouch`

--- packages/framework/src/input/stateChanges/MousePositionAbsoluteInputFromTouch.ts
+++ packages/framework/src/input/stateChanges/MousePositionAbsoluteInputFromTouch.ts
@@ -1,12 +1,15 @@
 import type { Vec2 } from '../../math';
 import type { TouchStateChangeEvent } from './events/TouchStateChangeEvent';
+import type { ISourcedFromTouch } from './ISourcedFromTouch';
 import { MousePositionAbsoluteInput } from './MousePositionAbsoluteInput';
 
-export class MousePositionAbsoluteInputFromTouch extends MousePositionAbsoluteInput {
+export class MousePositionAbsoluteInputFromTouch extends MousePositionAbsoluteInput implements ISourcedFromTouch {
   constructor(
     readonly touchEvent: TouchStateChangeEvent,
     position: Vec2,
   ) {
     super(position);
   }
+
+  readonly sourcedFromTouch = true;
 }
