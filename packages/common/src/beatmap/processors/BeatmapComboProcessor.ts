133366763386dc1e274a08a96c7e2f38a8ce2152
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-10 18:42:31 +0100

    Fix combo processor

--- packages/common/src/beatmap/processors/BeatmapComboProcessor.ts
+++ packages/common/src/beatmap/processors/BeatmapComboProcessor.ts
@@ -1,30 +1,30 @@
 import type { HitObject } from '../../hitObjects/HitObject';
-import { Color, type ReadonlyDependencyContainer, resolved } from 'osucad-framework';
 import { hasComboInformation } from '../../hitObjects/IHasComboInformation';
 import { OsuHitObject } from '../../rulesets/osu/hitObjects/OsuHitObject';
 import { Spinner } from '../../rulesets/osu/hitObjects/Spinner';
-import { ISkinSource } from '../../skinning/ISkinSource';
-import { SkinConfig } from '../../skinning/SkinConfig';
 import { BeatmapProcessor } from './BeatmapProcessor';
 
 export class BeatmapComboProcessor extends BeatmapProcessor {
   override onHitObjectAdded(hitObject: HitObject) {
     super.onHitObjectAdded(hitObject);
+
+    hitObject.startTimeBindable.valueChanged.addListener(this.state.invalidate, this.state);
+
     if (hitObject instanceof OsuHitObject) {
       hitObject.newComboBindable.valueChanged.addListener(this.state.invalidate, this.state);
       hitObject.comboOffsetBindable.valueChanged.addListener(this.state.invalidate, this.state);
     }
-    hitObject.startTimeBindable.valueChanged.addListener(this.state.invalidate, this.state);
   }
 
   override onHitObjectRemoved(hitObject: HitObject) {
     super.onHitObjectRemoved(hitObject);
 
+    hitObject.startTimeBindable.valueChanged.removeListener(this.state.invalidate, this.state);
+
     if (hitObject instanceof OsuHitObject) {
       hitObject.newComboBindable.valueChanged.removeListener(this.state.invalidate, this.state);
       hitObject.comboOffsetBindable.valueChanged.removeListener(this.state.invalidate, this.state);
     }
-    hitObject.startTimeBindable.valueChanged.removeListener(this.state.invalidate, this.state);
   }
 
   protected processBeatmap() {
@@ -50,58 +50,10 @@ export class BeatmapComboProcessor extends BeatmapProcessor {
         forceNewCombo = false;
       }
 
-      hitObject.comboColor = this.getComboColor(comboIndex);
       hitObject.comboIndex = comboIndex;
       hitObject.indexInCombo = indexInCombo;
 
       indexInCombo++;
     }
-  }
-
-  @resolved(ISkinSource)
-  protected skin!: ISkinSource;
-
-  #skinComboColors: readonly Color[] = [];
-
-  #comboColorsOverride: Color[] | null = null;
-
-  get comboColorsOverride() {
-    return this.#comboColorsOverride;
-  }
-
-  set comboColorsOverride(value: Color[] | null) {
-    if (value === this.#comboColorsOverride)
-      return;
-
-    this.#comboColorsOverride = value;
-    this.#skinChanged();
-  }
-
-  protected override load(dependencies: ReadonlyDependencyContainer) {
-    super.load(dependencies);
-
-    this.skin.sourceChanged.addListener(this.#skinChanged, this);
-    this.#skinChanged();
-  }
-
-  #skinChanged() {
-    this.#skinComboColors = this.skin.getConfig(SkinConfig.ComboColors) ?? [];
-    this.state.invalidate();
-  }
-
-  protected getComboColor(comboIndex: number) {
-    if (this.#comboColorsOverride !== null)
-      return this.#comboColorsOverride[comboIndex % this.#comboColorsOverride.length];
-
-    if (this.#skinComboColors.length > 0)
-      return this.#skinComboColors[comboIndex % this.#skinComboColors.length];
-
-    return new Color(0xFFFFFF);
-  }
-
-  override dispose(isDisposing: boolean = true) {
-    super.dispose(isDisposing);
-
-    this.skin.sourceChanged.removeListener(this.#skinChanged, this);
   }
 }
