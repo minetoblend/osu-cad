653f5bfb5975931b723c7d652dde1fa5a45bd7aa
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-13 22:05:50 +0100

    Fix container not showing up

--- packages/common/src/editor/ui/PrimaryActionContainer.ts
+++ packages/common/src/editor/ui/PrimaryActionContainer.ts
@@ -1,5 +1,5 @@
-import type { Drawable, HoverEvent, HoverLostEvent } from 'osucad-framework';
-import { Anchor, Axes, CompositeDrawable, Container, FastRoundedBox, FillDirection, FillFlowContainer, Vec2 } from 'osucad-framework';
+import type { Drawable, HoverEvent, HoverLostEvent, MouseDownEvent, MouseUpEvent } from 'osucad-framework';
+import { Anchor, Axes, CompositeDrawable, Container, EasingFunction, FastRoundedBox, FillDirection, FillFlowContainer, MouseButton, Vec2 } from 'osucad-framework';
 import { OsucadSpriteText } from '../../drawables/OsucadSpriteText';
 import { OsucadColors } from '../../OsucadColors';
 import { Corner } from './Corner';
@@ -8,6 +8,8 @@ import { EditorCornerContent } from './EditorCornerContent';
 export class PrimaryActionContainer extends EditorCornerContent {
   constructor() {
     super(Corner.TopLeft);
+
+    this.autoSizeAxes = Axes.X;
 
     this.padding = { horizontal: 6, vertical: 8 };
 
@@ -41,15 +43,13 @@ class TextButton extends CompositeDrawable {
   constructor(text: string) {
     super();
 
-    this.autoSizeAxes = Axes.Both;
-
     this.internalChildren = [
       this.#background = new FastRoundedBox({
         relativeSizeAxes: Axes.Both,
         alpha: 0,
         cornerRadius: 4,
       }),
-      new Container({
+      this.#content = new Container({
         autoSizeAxes: Axes.Both,
         padding: { horizontal: 8, vertical: 4 },
         child: this.#text = new OsucadSpriteText({
@@ -67,6 +67,8 @@ class TextButton extends CompositeDrawable {
 
   readonly #text: Drawable;
 
+  #content: Container;
+
   override onHover(e: HoverEvent): boolean {
     this.#background.fadeTo(0.1, 200);
     return true;
@@ -74,5 +76,23 @@ class TextButton extends CompositeDrawable {
 
   override onHoverLost(e: HoverLostEvent) {
     this.#background.fadeOut(100);
+  }
+
+  override get drawSize(): Vec2 {
+    return this.#content.drawSize;
+  }
+
+  override onMouseDown(e: MouseDownEvent): boolean {
+    if (e.button === MouseButton.Left)
+      this.#text.scaleTo(0.9, 300, EasingFunction.OutExpo);
+
+    return true;
+  }
+
+  override onMouseUp(e: MouseUpEvent) {
+    if (e.button === MouseButton.Left)
+      this.#text.scaleTo(1, 300, EasingFunction.OutBack);
+
+    return true;
   }
 }
