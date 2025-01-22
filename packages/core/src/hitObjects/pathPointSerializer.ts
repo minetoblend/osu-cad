ffc530136a96ba9da184c28f187ac349b0201020
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-22 05:08:45 +0100

    Apply rounding to path points

--- packages/core/src/hitObjects/pathPointSerializer.ts
+++ packages/core/src/hitObjects/pathPointSerializer.ts
@@ -5,7 +5,7 @@ import { PathPoint } from './PathPoint';
 
 export const pathPointSerializer: ISerializer<PathPoint, [number, number, PathType | null]> = {
   serialize(value) {
-    return [value.position.x, value.position.y, value.type];
+    return [Math.round(value.position.x * 10) / 10, Math.round(value.position.y * 10) / 10, value.type];
   },
   deserialize(value) {
     return new PathPoint(new Vec2(value[0], value[1]), value[2]);
