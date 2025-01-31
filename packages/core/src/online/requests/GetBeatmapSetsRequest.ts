920ac1f1e930651b43f4a012c6f2dbe4c521fdcc
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-31 16:05:42 +0100

    Fix lint error

--- packages/core/src/online/requests/GetBeatmapSetsRequest.ts
+++ packages/core/src/online/requests/GetBeatmapSetsRequest.ts
@@ -1,8 +1,8 @@
-import { APIRequest } from "./APIRequest";
-import { APIBeatmapSet } from "../APIBeatmapSet";
+import type { APIBeatmapSet } from '../APIBeatmapSet';
+import { APIRequest } from './APIRequest';
 
 export class GetBeatmapSetsRequest extends APIRequest<APIBeatmapSet[]> {
   constructor() {
-    super('/beatmapsets/')
+    super('/beatmapsets/');
   }
 }
