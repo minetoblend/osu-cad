c8ced8fee550d3cf2fa7fc0f6f1db14f211b2316
marvin <minetoblend@gmail.com>
2025-02-07 19:00:22 +0100

    Clean up deferred class

--- server/server-common/src/deferred.ts
+++ server/server-common/src/deferred.ts
@@ -1,9 +1,42 @@
-export class Deferred<T> {
-  resolve!: (value: T) => void;
-  reject!: (reason?: any) => void;
+export class Deferred<T> implements Promise<T> {
+  private _resolve!: (value: T | PromiseLike<T>) => void;
+  private _reject!: (reason: any) => void;
 
-  promise = new Promise<T>((resolve, reject) => {
-    this.resolve = resolve;
-    this.reject = reject;
-  });
+  readonly promise: Promise<T>;
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+
+  [Symbol.toStringTag]: string = 'Promise';
+
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?: ((value: T) => (PromiseLike<TResult1> | TResult1)) | undefined | null,
+    onrejected?: ((reason: any) => (PromiseLike<TResult2> | TResult2)) | undefined | null,
+  ): Promise<TResult1 | TResult2> {
+    return this.promise.then(onfulfilled, onrejected);
+  }
+
+  catch<TResult = never>(
+    onrejected?: ((reason: any) => (PromiseLike<TResult> | TResult)) | undefined | null,
+  ): Promise<T | TResult> {
+    return this.promise.catch(onrejected);
+  }
+
+  finally(
+    onfinally?: (() => void) | undefined | null,
+  ): Promise<T> {
+    return this.promise.finally(onfinally);
+  }
+
+  resolve(value: T | PromiseLike<T>): void {
+    this._resolve(value);
+  }
+
+  reject(reason?: any): void {
+    this._reject(reason);
+  }
 }
