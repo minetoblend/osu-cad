cf02f8806ed4579fcb656003317f2f66351b3b25
marvin <minetoblend@gmail.com>
2025-02-20 23:29:12 +0100

    Add export for selection manager

--- packages/ruleset-osu/src/edit/index.ts
+++ packages/ruleset-osu/src/edit/index.ts
@@ -7,6 +7,7 @@ export * from './IDistanceSnapProvider';
 export * from './IPositionSnapProvider';
 export * from './MobileEditorControls';
 export * from './OsuHitObjectComposer';
+export * from './OsuSelectionManager';
 export * from './OsuSelectTool';
 export * from './PlayfieldGrid';
 export * from './selection';
