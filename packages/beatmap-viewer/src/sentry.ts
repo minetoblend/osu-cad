7f3dc1d3902db706689f3313f565d156353eedc8
Marvin Schürz <m.schuerz@hautzy.com>
2025-02-24 17:26:16 +0100

    disable sentry replay integration

--- packages/beatmap-viewer/src/sentry.ts
+++ packages/beatmap-viewer/src/sentry.ts
@@ -5,8 +5,6 @@ if (import.meta.env.PROD) {
     dsn: 'https://393a6e668a783251d5de06d72596c077@o4506916793745408.ingest.us.sentry.io/4508596824965120',
     integrations: [
       Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration(),
-      Sentry.replayCanvasIntegration(),
     ],
     tracesSampleRate: 1.0,
     replaysSessionSampleRate: 0.05,
