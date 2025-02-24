e0031510df8caf3c2141879aeea906cbe4062f00
Marvin Schürz <m.schuerz@hautzy.com>
2025-02-24 17:47:40 +0100

    Disable sentry tracing integration

--- packages/beatmap-viewer/src/sentry.ts
+++ packages/beatmap-viewer/src/sentry.ts
@@ -3,11 +3,5 @@ import * as Sentry from '@sentry/browser';
 if (import.meta.env.PROD) {
   Sentry.init({
     dsn: 'https://393a6e668a783251d5de06d72596c077@o4506916793745408.ingest.us.sentry.io/4508596824965120',
-    integrations: [
-      Sentry.browserTracingIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 0.05,
-    replaysOnErrorSampleRate: 1.0,
   });
 }
