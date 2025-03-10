b7041d9ab05907f65d60f4ba08936d9d7652072c
marvin <minetoblend@gmail.com>
2025-03-11 00:43:23 +0100

    Adjust cursor texture offset

--- packages/core/src/graphics/cursor/DefaultCursorContainer.ts
+++ packages/core/src/graphics/cursor/DefaultCursorContainer.ts
@@ -17,22 +17,26 @@ export class DefaultCursor extends CompositeDrawable {
     this.addAllInternal(
       this.#shadow = new DrawableSprite({
         texture: getIcon('select'),
-        x: -4,
+        x: -5,
         y: -1,
         color: 0x000000,
         alpha: 0.2,
       }),
       this.#sprite = new DrawableSprite({
         texture: getIcon('select'),
-        x: -5,
+        x: -6,
         y: -4,
       }),
     );
   }
 
-  #shadow!: DrawableSprite;
+  readonly #shadow: DrawableSprite;
 
-  #sprite!: DrawableSprite;
+  readonly #sprite!: DrawableSprite;
+
+  override receivePositionalInputAt(screenSpacePosition: Vec2): boolean {
+    return true;
+  }
 
   protected get sprite() {
     return this.#sprite;
@@ -51,17 +55,17 @@ export class DefaultCursor extends CompositeDrawable {
 
   animateMouseDown() {
     this.#sprite.scaleTo(0.8, 1000, EasingFunction.OutQuart);
-    this.#sprite.moveTo(new Vec2(-5, -4), 1000, EasingFunction.OutQuart);
+    this.#sprite.moveTo(new Vec2(-6, -4), 1000, EasingFunction.OutQuart);
 
     this.#shadow.scaleTo(0.8, 1000, EasingFunction.OutQuart);
-    this.#shadow.moveTo(new Vec2(-5, -2), 1000, EasingFunction.OutQuart);
+    this.#shadow.moveTo(new Vec2(-7, -2), 1000, EasingFunction.OutQuart);
   }
 
   animateMouseUp() {
     this.#sprite.scaleTo(1, 200, EasingFunction.OutBack);
-    this.#sprite.moveTo(new Vec2(-4, -3), 200, EasingFunction.OutBack);
+    this.#sprite.moveTo(new Vec2(-5, -3), 200, EasingFunction.OutBack);
 
     this.#shadow.scaleTo(1, 200, EasingFunction.OutBack);
-    this.#shadow.moveTo(new Vec2(-4, -1), 200, EasingFunction.OutBack);
+    this.#shadow.moveTo(new Vec2(-6, -1), 200, EasingFunction.OutBack);
   }
 }
