c0271372783090f093344107709677320c0ccead
marvin <minetoblend@gmail.com>
2025-04-07 22:14:39 +0200

    Only enable posthog in prod mode

--- packages/beatmap-viewer/src/posthog.ts
+++ packages/beatmap-viewer/src/posthog.ts
@@ -1,6 +1,8 @@
 import posthog from 'posthog-js';
 
-posthog.init('phc_G783rtb7P2zMex7Mljk5nESMUDYwcGWGg3t0m6dNNKf', {
-  api_host: 'https://eu.i.posthog.com',
-  person_profiles: 'never',
-});
+if (import.meta.env.PROD) {
+  posthog.init('phc_G783rtb7P2zMex7Mljk5nESMUDYwcGWGg3t0m6dNNKf', {
+    api_host: 'https://eu.i.posthog.com',
+    person_profiles: 'never',
+  });
+}
