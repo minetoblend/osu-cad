a97a15955e01133b019a665a80091cad58786c40
marvin <minetoblend@gmail.com>
2025-06-01 02:20:47 +0200

    Remove unused import

--- packages/ruleset-osu/src/hitObjects/drawables/DrawableSpinnerBonusTick.ts
+++ packages/ruleset-osu/src/hitObjects/drawables/DrawableSpinnerBonusTick.ts
@@ -1,4 +1,3 @@
-import { DrawableOsuHitObject } from "./DrawableOsuHitObject";
 import { DrawableSpinnerTick } from "./DrawableSpinnerTick";
 
 export class DrawableSpinnerBonusTick extends DrawableSpinnerTick
