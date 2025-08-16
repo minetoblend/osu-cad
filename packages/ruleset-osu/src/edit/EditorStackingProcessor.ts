bc3f796a07156168a919d9540c08cd702fe9cbb3
marvin <minetoblend@gmail.com>
2025-08-16 22:06:00 +0200

    Fix stacking not being updated when hitobject gets added

--- packages/ruleset-osu/src/edit/EditorStackingProcessor.ts
+++ packages/ruleset-osu/src/edit/EditorStackingProcessor.ts
@@ -2,12 +2,22 @@ import type { EditorBeatmap } from "@osucad/editor";
 import { EditorBeatmapProcessor } from "@osucad/editor";
 import type { OsuHitObject } from "../hitObjects";
 import { calculateStacking } from "../stacking";
+import type { HitObject } from "@osucad/core";
 
 export class EditorStackingProcessor extends EditorBeatmapProcessor
 {
   constructor()
   {
     super(["stacking"]);
+  }
+
+  protected override onHitObjectAdded(hitObject: HitObject): void
+  {
+    this.refresh();
+  }
+
+  protected override onHitObjectRemoved(hitObject: HitObject): void {
+    this.refresh();
   }
 
   protected override process(beatmap: EditorBeatmap): void
