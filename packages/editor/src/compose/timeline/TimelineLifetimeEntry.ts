d2b9d02a54043d9d11f81f29c485cdcc7d30c133
Marvin Schürz <m.schuerz@hautzy.com>
2025-09-03 09:16:42 +0200

    Fix linter errors

--- packages/editor/src/compose/timeline/TimelineLifetimeEntry.ts
+++ packages/editor/src/compose/timeline/TimelineLifetimeEntry.ts
@@ -2,8 +2,10 @@ import { HitObjectLifetimeEntry, type HitObject } from "@osucad/core";
 import { Bindable } from "@osucad/framework";
 
 
-export class TimelineLifetimeEntry extends HitObjectLifetimeEntry {
-  public constructor(hitObject: HitObject) {
+export class TimelineLifetimeEntry extends HitObjectLifetimeEntry
+{
+  public constructor(hitObject: HitObject)
+  {
     super(hitObject);
 
     hitObject.defaultsApplied.addListener(this.setInitialLifetime, this);
@@ -11,12 +13,14 @@ export class TimelineLifetimeEntry extends HitObjectLifetimeEntry {
 
   public readonly selected = new Bindable(false);
 
-  protected override setInitialLifetime(): void {
+  protected override setInitialLifetime(): void
+  {
     super.setInitialLifetime();
     this.lifetimeEnd = this.hitObject.endTime;
   }
 
-  public override get initialLifetimeOffset(): number {
+  public override get initialLifetimeOffset(): number
+  {
     return 0;
   }
 }
