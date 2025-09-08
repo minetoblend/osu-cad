5667b5dbec28307e951bea6560869b12e109faed
Marvin Schürz <m.schuerz@hautzy.com>
2025-09-09 01:23:27 +0200

    configure endpoint via .env

--- apps/editor-web/src/EditorLoader.ts
+++ apps/editor-web/src/EditorLoader.ts
@@ -49,7 +49,7 @@ export class EditorLoader extends Screen
     const { MultiplayerClient } = await import("@osucad/multiplayer-client");
 
     const client = new MultiplayerClient({
-      endpoint: "http://localhost:3000",
+      endpoint: import.meta.env.VITE_API_BASEURL,
     });
 
     this.#document = await client.load("beatmap", { runtimeFactory: editorRuntimeFactory });
--- apps/editor-web/src/vite-env.d.ts
+++ apps/editor-web/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv
+{
+  readonly VITE_API_BASEURL: string
+}
+
+interface ImportMeta
+{
+  readonly env: ImportMetaEnv
+}
+
