176ba1ee6ee78c28360bc8bf7bb79e8f8f646102
Marvin Schürz <m.schuerz@hautzy.com>
2025-09-08 23:26:07 +0200

    refactor LoadingSpinner

--- apps/editor-web/src/LoadingSpinner.ts
+++ apps/editor-web/src/LoadingSpinner.ts
@@ -1,36 +1,32 @@
 import type { DrawableOptions } from "@osucad/framework";
-import { Drawable, EasingFunction } from "@osucad/framework";
-import { Graphics } from "pixi.js";
+import { EasingFunction, GraphicsDrawable } from "@osucad/framework";
+import type { Graphics } from "pixi.js";
+import { EditorColors } from "@osucad/editor";
 
-export class LoadingSpinner extends Drawable
+export class LoadingSpinner extends GraphicsDrawable
 {
   public constructor(options: DrawableOptions = {})
   {
     super();
     this.with({
-      color: 0x52CCA3,
+      color: EditorColors.primary,
       ...options,
     });
   }
-
-  public createDrawNode()
-  {
-    return this.#graphics = new Graphics();
-  }
-
-  #graphics!: Graphics;
 
   protected override update()
   {
     super.update();
 
-    this.#updateGraphics(this.#graphics);
+    this.invalidateGraphics();
   }
 
   public animationSpeed = 2;
 
-  #updateGraphics(g: Graphics)
+  protected override updateGraphics(g: Graphics): void
   {
+    g.clear();
+
     function animate(time: number, offset = 0)
     {
       const t = (time / 1000 + offset) % 1;
@@ -42,15 +38,13 @@ export class LoadingSpinner extends Drawable
     let endAngle = animate(this.time.current * this.animationSpeed, 0.3);
 
     if (endAngle < startAngle)
-    {
       endAngle += Math.PI * 2;
-    }
 
     const drawSize = this.drawSize;
     const radius = Math.min(drawSize.x, drawSize.y) / 2;
 
-    g.clear()
-      .arc(drawSize.x / 2, this.drawSize.y / 2, radius, startAngle, endAngle)
+
+    g.arc(drawSize.x / 2, this.drawSize.y / 2, radius, startAngle, endAngle)
       .stroke({
         color: 0xFFFFFF,
         width: radius * 0.25,
