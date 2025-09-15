3ed31f84211168d86bdbc03281f6865abf1b4469
marvin <minetoblend@gmail.com>
2025-09-15 20:46:30 +0200

    fix bug in timestamp formatter

--- packages/editor/src/utils/TimestampFormatter.ts
+++ packages/editor/src/utils/TimestampFormatter.ts
@@ -8,7 +8,7 @@ export namespace TimestampFormatter
   export function format(time: number)
   {
     const minutes = Math.floor(time / 60_000);
-    const seconds = Math.floor(time / 1000) % 1000;
+    const seconds = Math.floor(time / 1000) % 60;
     const milliseconds = Math.floor(time % 1000);
 
     return `${padZeroes(minutes, 2)}:${padZeroes(seconds, 2)}:${padZeroes(milliseconds,3)}`;
