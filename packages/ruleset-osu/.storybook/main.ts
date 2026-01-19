19583fa9aedf5fba3d6fb28a44f72aba58d1a5c9
marvin <m.schuerz@hautzy.com>
2026-01-19 18:08:13 +0100

    fix linter errors

--- packages/ruleset-osu/.storybook/main.ts
+++ packages/ruleset-osu/.storybook/main.ts
@@ -25,6 +25,7 @@ const config: StorybookConfig = {
 
 export default config;
 
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): any
+{
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
